d729af662053bd9c58487d1b20f2945c4d840f8c
Emil212 <79732590+Emil212@users.noreply.github.com>
2022-02-22 06:25:42 -0600

    Firebase auth email and password complete

--- app/src/main/java/com/example/menuprueba/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/menuprueba/ui/auth/LoginFragment.kt
@@ -3,11 +3,18 @@ package com.example.menuprueba.ui.auth
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.util.PatternsCompat
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.menuprueba.R
+import com.example.menuprueba.core.Resource
+import com.example.menuprueba.data.remote.auth.LoginDataSource
 import com.example.menuprueba.databinding.FragmentLoginBinding
+import com.example.menuprueba.domain.auth.LoginRepoImpl
+import com.example.menuprueba.presentation.auth.LoginScreenViewModel
+import com.example.menuprueba.presentation.auth.LoginScreenViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
 import java.util.regex.Pattern
 
@@ -15,6 +22,13 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
     private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val viewModel by viewModels<LoginScreenViewModel> {
+        LoginScreenViewModelFactory(
+            LoginRepoImpl(
+                LoginDataSource()
+            )
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,6 +49,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             val email = binding.editTextEmail.text.toString().trim()
             val password = binding.editTextPassword.text.toString().trim()
             validateCredentials(email, password)
+            signIn(email, password)
         }
     }
 
@@ -58,18 +73,39 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             binding.editTextEmail.error = "La contraseña esta en blanco"
             return
         }
-        if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()) {
-            binding.editTextEmail.error = "Ingrese un correo electrónico válido"
-            return
-        }
-        if (!passwordRegex.matcher(password).matches()) {
-            binding.editTextEmail.error =
-                "La contraeña debe de contener al menos 4 caracteres, una mayuscula, una minuscula, un numero y un caracter especial"
-            return
-        }
+//        if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()) {
+//            binding.editTextEmail.error = "Ingrese un correo electrónico válido"
+//            return
+//        }
+//        if (!passwordRegex.matcher(password).matches()) {
+//            binding.editTextEmail.error =
+//                "La contraeña debe de contener al menos 4 caracteres, una mayuscula, una minuscula, un numero y un caracter especial"
+//            return
+//        }
     }
 
     private fun signIn(email: String, password: String) {
+        viewModel.signIn(email, password).observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.btnSignin.isEnabled = false
+                }
+                is Resource.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    findNavController().navigate(R.id.action_loginFragment_to_nav_rutinas)
+                }
+                is Resource.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.btnSignin.isEnabled = true
+                    Toast.makeText(
+                        requireContext(),
+                        "Error: ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
 
     }
 
