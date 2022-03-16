40540b215ab1521224017ee29d96523e7514f739
ariel-41d4n4 <aaldanap1700@alumno.ipn.mx>
2022-03-16 05:58:34 -0600

    Temporizador agragado AAP

--- app/src/main/java/com/example/menuprueba/ui/rutinas/VideosFragment.kt
+++ app/src/main/java/com/example/menuprueba/ui/rutinas/VideosFragment.kt
@@ -3,6 +3,7 @@ package com.example.menuprueba.ui.rutinas
 import android.net.Uri
 import android.os.Binder
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -30,12 +31,34 @@ class VideosFragment : Fragment(R.layout.fragment_videos) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentVideosBinding.bind(view)
-        seeImage()
+        seeImagePreview()
+        contador()
+    }
+
+    private fun contador() {
+        val seeTime = binding.time
+        val iniciar = binding.iniciar
+        iniciar.setOnClickListener {
+            val duracion = 30000
+            object : CountDownTimer (duracion.toLong(), 1000){
+                override fun onTick(p0: Long) {
+                    seeImage()
+                    val segundos = (p0/1000) % 60
+                    val mostrar = String.format("%02d", segundos)
+                    seeTime.setText("$mostrar s")
+                }
+
+                override fun onFinish() {
+                    seeTime.setText("Se acabó")
+                    seeImagePreview()
+                }
+            }.start()
+        }
     }
 
     fun seeImage() {
-        val Ref = Firebase.storage.reference //Hace referencia a la úbicación del proyecto
-        val a = Ref.child("Flexibilidad/saludo-al-sol-1600959006.gif").downloadUrl
+        //val Ref = Firebase.storage.reference //Hace referencia a la úbicación del proyecto
+        //val a = Ref.child("Flexibilidad/saludo-al-sol-1600959006.gif").downloadUrl
         val imageview = binding.imageView
         val ImagesFlex =
             "https://firebasestorage.googleapis.com/v0/b/gamificationapp-2ff7c.appspot.com/o/Flexibilidad%2Fsaludo-al-sol-1600959006.gif?alt=media&token=2101ddb3-380c-4a75-b445-1db9fad3cf5b"
@@ -43,10 +66,24 @@ class VideosFragment : Fragment(R.layout.fragment_videos) {
         Glide
             .with(this)
             .load(ImagesFlex)
-            .fitCenter()
-            .centerCrop()
+            //.fitCenter()
+            //.centerCrop()
             .into(imageview)
-
-        Log.d("Prueba de enlace Desc", "$a")
     }
+
+    fun seeImagePreview() {
+        //val Ref = Firebase.storage.reference //Hace referencia a la úbicación del proyecto
+        //val a = Ref.child("Flexibilidad/saludo-al-sol-1600959006.gif").downloadUrl
+        val imageview = binding.imageView
+        val ImagesFlex =
+            "https://firebasestorage.googleapis.com/v0/b/gamificationapp-2ff7c.appspot.com/o/Flexibilidad%2F2022-03-16%20(3).png?alt=media&token=4715dac6-ef0e-4f9e-b3b4-88551e471d60"
+        //                Enlace de descarga (Token de Acceso)
+        Glide
+            .with(this)
+            .load(ImagesFlex)
+            //.fitCenter()
+            //.centerCrop()
+            .into(imageview)
+    }
+
 }
